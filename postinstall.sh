f48e72d22015b3d1204114a0c48c2e90339be247
maxmcd <max.t.mcdonnell@gmail.com>
2018-05-11 08:43:06 -0400

    Add postinstall hook

---
+++ postinstall.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+echo "Downloading ijkplayer iOS framework (145M)"
+curl https://storage.googleapis.com/rivet/IJKMediaFramework \
+    -o ios/IJKMediaFramework.framework/IJKMediaFramework \
+    -#
